d5016af55e4231053a3f683233912af29289bf10
Fabricio Guimarães <79614829+Fabrowser@users.noreply.github.com>
2022-07-31 19:56:50 -0300

    alterado os segundos de 58 para 59 para teste

--- DateTime_kind/Program.cs
+++ DateTime_kind/Program.cs
@@ -7,10 +7,10 @@ namespace DateTime_kind
         static void Main(string[] args)
         {
 
-            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
-            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
-            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
-            DateTime d4 = new DateTime(2000, 8, 15, 13, 5, 58);
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 59, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 59, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 59);
+            DateTime d4 = new DateTime(2000, 8, 15, 13, 5, 59);
 
 
 
